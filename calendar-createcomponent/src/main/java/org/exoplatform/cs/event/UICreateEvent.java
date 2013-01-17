9f70425fddc5140e2edfae1746387a94a14d121c
Tuan Pham <tuanp@exoplatform.com>
2013-01-17 13:57:48 +0700

    CAL-184 split date and time, check all day event created

--- calendar-createcomponent/src/main/java/org/exoplatform/cs/event/UICreateEvent.java
+++ calendar-createcomponent/src/main/java/org/exoplatform/cs/event/UICreateEvent.java
@@ -1,18 +1,24 @@
 package org.exoplatform.cs.event;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 import java.util.Set;
+import java.util.TimeZone;
 
 import org.exoplatform.calendar.service.CalendarEvent;
 import org.exoplatform.calendar.service.CalendarService;
+import org.exoplatform.calendar.service.CalendarSetting;
 import org.exoplatform.calendar.service.GroupCalendarData;
 import org.exoplatform.calendar.service.Utils;
 import org.exoplatform.calendar.service.impl.NewUserListener;
@@ -40,6 +46,7 @@ import org.exoplatform.webui.event.EventListener;
 import org.exoplatform.webui.form.UIForm;
 import org.exoplatform.webui.form.UIFormDateTimeInput;
 import org.exoplatform.webui.form.UIFormRadioBoxInput;
+import org.exoplatform.webui.form.UIFormSelectBox;
 import org.exoplatform.webui.form.UIFormSelectBoxWithGroups;
 import org.exoplatform.webui.form.UIFormStringInput;
 
@@ -81,25 +88,33 @@ public class UICreateEvent extends UIForm {
   public static final String SLASH_COLON = "/:";
   public static final String OPEN_PARENTHESIS = "(";
   public static final String CLOSE_PARENTHESIS = ")";
-  static List<SelectItemOption<String>> options = new ArrayList();
+  static List<SelectItemOption<String>> options = new ArrayList<SelectItemOption<String>>();
   private static Log log = ExoLogger.getLogger(UICreateEvent.class);
   static String CHOIX = "Choix";
 
   static String TITLE = "Title";
 
-  static String END_EVENT = "EndEvent";
-  static String CALENDAR = "Calendar";
-  static String Start_EVENT = "StartEvent";
+  public static String END_EVENT = "EndEvent";
+  public static String CALENDAR = "Calendar";
+  public static String Start_EVENT = "StartEvent";
+  public  static String START_TIME = "start_time";
+  public static String END_TIME = "end_time";
+  public static String ALL_DAY = "all-day";
   private String calType_ = "0";
+  public static final String TIMEFORMAT  = "HH:mm" ;
+  public static final String DISPLAY_TIMEFORMAT = "HH:mm a";
+  public static final long DEFAULT_TIME_INTERVAL = 30;
   public UICreateEvent()
       throws Exception {
 
 
     addUIFormInput(new UIFormRadioBoxInput(CHOIX, "Event", options));
     addUIFormInput(new UIFormStringInput(TITLE, TITLE, null));
-    addUIFormInput(new UIFormDateTimeInput(Start_EVENT, Start_EVENT, null, true));
-    addUIFormInput(new UIFormDateTimeInput(END_EVENT, END_EVENT, null, true));
+    addUIFormInput(new UIFormDateTimeInput(Start_EVENT, Start_EVENT, null, false));
+    addUIFormInput(new UIFormDateTimeInput(END_EVENT, END_EVENT, null, false));
     addUIFormInput(new UIFormSelectBoxWithGroups(CALENDAR, CALENDAR, getCalendarOption()));
+    addUIFormInput(new UIFormSelectBox(START_TIME, START_TIME, getTimesSelectBoxOptions(DISPLAY_TIMEFORMAT))) ;
+    addUIFormInput(new UIFormSelectBox(END_TIME, END_TIME, getTimesSelectBoxOptions(DISPLAY_TIMEFORMAT))) ;
 
 
   }
@@ -128,8 +143,8 @@ public class UICreateEvent extends UIForm {
       summary = enCodeTitle(summary);
       UIFormDateTimeInput fromField = uiForm.getChildById(Start_EVENT);
       UIFormDateTimeInput toField = uiForm.getChildById(END_EVENT);
-      Calendar from =  fromField.getCalendar();
-      Calendar to = toField.getCalendar();
+      Date from =  uiForm.getDateTime(fromField, UICreateEvent.START_TIME);
+      Date to = uiForm.getDateTime(toField, UICreateEvent.END_TIME);
       if(from == null) {
         event.getRequestContext().getUIApplication().addMessage(new ApplicationMessage(uiForm.getId() + ".msg.fromDate-format", null, ApplicationMessage.WARNING)) ;
         ((PortalRequestContext) event.getRequestContext().getParentAppRequestContext()).setFullRender(true);
@@ -169,10 +184,9 @@ public class UICreateEvent extends UIForm {
           calEvent.setEventState(CalendarEvent.NEEDS_ACTION) ;
           calEvent.setTaskDelegator(event.getRequestContext().getRemoteUser());
         }
-        calEvent.setFromDateTime(from.getTime());
-        calEvent.setToDateTime(to.getTime()) ;
+        calEvent.setFromDateTime(from);
+        calEvent.setToDateTime(to) ;
         calEvent.setCalType(uiForm.calType_) ;
-        
           
         if(uiForm.calType_.equals(PRIVATE_TYPE)) {
           calService.saveUserEvent(username, calEvent.getCalendarId(), calEvent, true) ;
@@ -198,7 +212,104 @@ public class UICreateEvent extends UIForm {
     }
   }
 
+  public Date getDateTime(UIFormDateTimeInput input, String selectId) throws Exception{
+    String timeField = getUIFormSelectBox(selectId).getValue();
+    boolean isAllDate = ALL_DAY.equals(timeField);
+    if(END_TIME.equals(selectId)) {
+      return getEndDate(isAllDate, input.getDatePattern_(), input.getValue(), TIMEFORMAT, timeField);
+    } else return getBeginDate(isAllDate, input.getDatePattern_(), input.getValue(), TIMEFORMAT, timeField);
+  }
+  
+  public static Date getBeginDate(boolean isAllDate,String dateFormat, String fromField,String timeFormat,String timeField) throws Exception {
+    WebuiRequestContext context = WebuiRequestContext.getCurrentInstance() ;
+    Locale locale = context.getParentAppRequestContext().getLocale() ;
+    if(isAllDate) {
+      DateFormat df = new SimpleDateFormat(dateFormat, locale) ;
+      df.setCalendar(getInstanceOfCurrentCalendar()) ;
+      return getBeginDay(df.parse(fromField)).getTime();
+    } 
+    DateFormat df = new SimpleDateFormat(dateFormat + Utils.SPACE  + timeFormat, locale) ;
+    df.setCalendar(getInstanceOfCurrentCalendar()) ;
+    return df.parse(fromField + Utils.SPACE + timeField) ;
+  }
+  
+  public static Date getEndDate(boolean isAllDate,String dateFormat, String fromField,String timeFormat,String timeField) throws Exception {
+    WebuiRequestContext context = WebuiRequestContext.getCurrentInstance() ;
+    Locale locale = context.getParentAppRequestContext().getLocale() ;
+    if(isAllDate) {
+      DateFormat df = new SimpleDateFormat(dateFormat, locale) ;
+      df.setCalendar(getInstanceOfCurrentCalendar()) ;
+      return getEndDay(df.parse(fromField)).getTime();
+    } 
+    DateFormat df = new SimpleDateFormat(dateFormat + Utils.SPACE  + timeFormat, locale) ;
+    df.setCalendar(getInstanceOfCurrentCalendar()) ;
+    return df.parse(fromField + Utils.SPACE + timeField) ;
+  }
 
+public static CalendarSetting getCurrentUserCalendarSetting() {
+    try {
+      String user = getCurrentUser();
+        CalendarSetting setting = getCalendarService().getCalendarSetting(user) ;
+      return setting;
+    } catch (Exception e) {
+      log.warn("could not get calendar setting of user", e);
+      return null;
+    }
+      
+  }
+  
+  public static Calendar getInstanceOfCurrentCalendar() {
+    try {
+     CalendarSetting setting = getCurrentUserCalendarSetting();
+     return getCalendarInstanceBySetting(setting); 
+   } catch (Exception e) {
+     if (log.isWarnEnabled()) log.warn("Could not get calendar setting!", e);
+     Calendar calendar = GregorianCalendar.getInstance() ;
+     calendar.setLenient(false);
+     return calendar;
+   }
+ }
+  public static Calendar getCalendarInstanceBySetting(final CalendarSetting calendarSetting) {
+    Calendar  calendar = GregorianCalendar.getInstance() ;
+    calendar.setLenient(false);
+    calendar.setTimeZone(TimeZone.getTimeZone(calendarSetting.getTimeZone()));
+    calendar.setFirstDayOfWeek(Integer.parseInt(calendarSetting.getWeekStartOn()));
+    // fix CS-4725
+    calendar.setMinimalDaysInFirstWeek(4);
+    return calendar;
+  }
+  
+  public static Calendar getBeginDay(Calendar cal) {
+    Calendar newCal = (Calendar) cal.clone();
+    
+    newCal.set(Calendar.HOUR_OF_DAY, 0) ;
+    newCal.set(Calendar.MINUTE, 0) ;
+    newCal.set(Calendar.SECOND, 0) ;
+    newCal.set(Calendar.MILLISECOND, 0) ;
+    return newCal ;
+  }
+  public static Calendar getEndDay(Calendar cal)  {
+    Calendar newCal = (Calendar) cal.clone();    
+    newCal.set(Calendar.HOUR_OF_DAY, 0) ;
+    newCal.set(Calendar.MINUTE, 0) ;
+    newCal.set(Calendar.SECOND, 0) ;
+    newCal.set(Calendar.MILLISECOND, 0) ;
+    newCal.add(Calendar.HOUR_OF_DAY, 24) ;
+    return newCal ;
+  }
+
+  public static Calendar getBeginDay(Date date) {    
+    Calendar cal = getInstanceOfCurrentCalendar() ;
+    cal.setTime(date) ;
+    return getBeginDay(cal) ;
+  }
+  
+  public static Calendar getEndDay(Date date)  {
+    Calendar cal = getInstanceOfCurrentCalendar() ;
+    cal.setTime(date) ;
+    return getEndDay(cal) ;
+  }
+  
   static public class CancelActionListener extends EventListener<UICreateEvent> {
 
 
@@ -220,7 +331,45 @@ public class UICreateEvent extends UIForm {
     options.add(new SelectItemOption("Event"));
     options.add(new SelectItemOption("Task"));
   }
+  
+  public static List<SelectItemOption<String>> getTimesSelectBoxOptions(String timeFormat) {
+    WebuiRequestContext context = WebuiRequestContext.getCurrentInstance() ;
+    Locale locale = context.getParentAppRequestContext().getLocale() ;
+    return getTimesSelectBoxOptions(timeFormat, TIMEFORMAT, DEFAULT_TIME_INTERVAL, locale);
+  }
+  
+ public static List<SelectItemOption<String>> getTimesSelectBoxOptions(String labelFormat, String valueFormat, long timeInteval) {
+    
+    WebuiRequestContext context = WebuiRequestContext.getCurrentInstance() ;
+    Locale locale = context.getParentAppRequestContext().getLocale() ;
+    return getTimesSelectBoxOptions(labelFormat, valueFormat, timeInteval, locale);
+  }
+ 
+ public static List<SelectItemOption<String>> getTimesSelectBoxOptions(String labelFormat, String valueFormat, long timeInteval, Locale locale) {
+   List<SelectItemOption<String>> options = new ArrayList<SelectItemOption<String>>() ;
+   options.add(new SelectItemOption<String>("All Day", ALL_DAY)) ;
+   Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("")); // get a GMT calendar
+   cal.set(Calendar.HOUR_OF_DAY, 0) ;
+   cal.set(Calendar.MINUTE, 0) ;
+   cal.set(Calendar.MILLISECOND, 0) ;
 
+   DateFormat dfLabel = new SimpleDateFormat(labelFormat, locale) ;
+   dfLabel.setCalendar(cal) ;
+   DateFormat dfValue = new SimpleDateFormat(valueFormat, locale) ;
+   dfValue.setCalendar(cal) ;
+   
+   int day = cal.get(Calendar.DAY_OF_MONTH);
+   while (day == cal.get(Calendar.DAY_OF_MONTH)) {
+     options.add(new SelectItemOption<String>(dfLabel.format(cal.getTime()), dfValue.format(cal.getTime()))) ;
+     cal.add(java.util.Calendar.MINUTE, (int)timeInteval) ;
+   }
+   cal.set(Calendar.DAY_OF_MONTH, day);
+   cal.set(Calendar.HOUR_OF_DAY, 23);
+   cal.set(Calendar.MINUTE, 59);
+   cal.set(Calendar.MILLISECOND, 59) ;
+   options.add(new SelectItemOption<String>(dfLabel.format(cal.getTime()), dfValue.format(cal.getTime()))) ;
+   return options ;
+ }
   public static List<SelectItem> getCalendarOption() throws Exception {
     List<SelectItem> options = new ArrayList<SelectItem>() ;
     CalendarService calendarService = getCalendarService() ;
