e43123b564c7d7ee13ec60644c29d3d3a31de4f8
Tuan Pham <tuanp@exoplatform.com>
2013-01-22 11:34:41 +0700

    CAL-194 by default quick add task have no reminder

--- calendar-webapp/src/main/java/org/exoplatform/calendar/webui/popup/UIEventReminderTab.java
+++ calendar-webapp/src/main/java/org/exoplatform/calendar/webui/popup/UIEventReminderTab.java
@@ -88,7 +88,7 @@ public class UIEventReminderTab extends UIFormInputWithActions {
 
     List<SelectItemOption<String>> emailRemindRepeatOptions = getReminderTimes(5,60) ;
     List<SelectItemOption<String>> emailRemindBeforeOptions = getReminderTimes(5,60) ;
-    addUIFormInput(new UICheckBoxInput(REMIND_BY_EMAIL, REMIND_BY_EMAIL, true)) ;
+    addUIFormInput(new UICheckBoxInput(REMIND_BY_EMAIL, REMIND_BY_EMAIL, false)) ;
     addUIFormInput(new UIFormSelectBox(EMAIL_REMIND_BEFORE, EMAIL_REMIND_BEFORE, emailRemindBeforeOptions));
     addUIFormInput(new UIEmailInput(FIELD_EMAIL_ADDRESS, FIELD_EMAIL_ADDRESS, null)) ;
     addUIFormInput(new UICheckBoxInput(EMAIL_IS_REPEAT, EMAIL_IS_REPEAT, false));
