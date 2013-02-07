6193059042b4b0d9e0fdd2932b095913de7ef8b5
Tuan Pham <tuanp@exoplatform.com>
2013-02-07 17:56:06 +0700

    CAL-254 add rule support i18n

--- calendar-resources/src/main/webapp/javascript/eXo/cs/TimePicker.js
+++ calendar-resources/src/main/webapp/javascript/eXo/cs/TimePicker.js
@@ -1,6 +1,5 @@
-(function(base, gj){
+(function(uiCalendar, base, gj){
 var _module = {};
-
 function TimePicker() {
    
 } ;
@@ -40,11 +39,27 @@ TimePicker.prototype.initTime = function(id) {
 
 TimePicker.prototype.updateEndInput = function(value) {
 	var endTime  = gj('#endTimeId'); 
-	if(endTime) {
-		value = _module.TimePicker.addHour(value, 1);
+	var startTime  = gj('#startTimeId'); 
+	if(startTime) {
+		var selectBox = gj(startTime).find('select')[0];
+		var start = gj(selectBox).val();
+		 if(start == '23:00') {
+		 	_module.TimePicker.addDay(1);
+		 	value = '00:00';
+		 } else if(start == '23:30') {
+		 	_module.TimePicker.addDay(1);
+		 	value = '00:30';
+		 } else if(start == '23:59') {
+		 	_module.TimePicker.addDay(1);
+		 	value = '01:00';
+		 } else {
+	 		_module.TimePicker.addDay(0);	
+			value = _module.TimePicker.addHour(value, 1);
+		 }
 		var selectBox = gj(endTime).find('select')[0];
 		gj(selectBox).find('option[value="'+ value +'"]').attr('selected','true');
 	}
+	
 }
 
 
@@ -58,7 +73,7 @@ TimePicker.prototype.selectTime = function() {
 	var value =  gj(this).val();
 	var endTime  = gj('#endTimeId'); 
 	if(endTime) {
-		if(value != 'all-day') value = _module.TimePicker.addHour(value, 1);
+		if(value != 'all-day') value = _module.TimePicker.updateEndInput(value);
 		var selectBox = gj(endTime).find('select')[0];
 		gj(selectBox).find('option[value="'+value+'"]').attr('selected','true');
 	}
@@ -71,7 +86,9 @@ TimePicker.prototype.addHour = function(input, interval) {
 		hour = parseInt(hourStr[1]);
 	else 
 		hour = parseInt(hourStr);
-	if(hour >= 23) return "23:59";
+	if(hour >= 23) {
+		return "23:59";
+	}
 	hour += interval;
 	if(hour < 10) hour = "0" + hour;
 	return  hour+':'+input.split(':')[1];
@@ -84,19 +101,45 @@ TimePicker.prototype.selectDate = function() {
 TimePicker.prototype.addDate = function() {
 	 var startDateInput = gj('#startDateId input');
 	 var endDateInput = gj('#endDateId input');
-	 endDateInput.val(startDateInput.val());
 	 var startTime  = gj('#startTimeId'); 
 		if(startTime) {
 			var selectBox = gj(startTime).find('select')[0];
 			if(selectBox) {
-				_module.TimePicker.updateEndInput(gj(selectBox).val());
+				var start = gj(selectBox).val();
+				_module.TimePicker.updateEndInput(start);
+
 			}
 
 		}
 	 
 }
 
+TimePicker.prototype.addDay = function(dayNum) {
+	 var startDateInput = gj('#startDateId input');
+	 var endDateInput = gj('#endDateId input');
+	 var dateValue = startDateInput.val();
+	 uiCalendar.currentDate = _module.TimePicker.dateParses(dateValue, _module.TimePicker.datePattern);
+	 uiCalendar.currentDate.setDate(uiCalendar.currentDate.getDate()+dayNum);
+	 uiCalendar.datePattern = _module.TimePicker.datePattern;
+	 var value = uiCalendar.getDateTimeString();
+	 endDateInput.val(value);	 
+}
+
+TimePicker.prototype.dateParses = function(dateFieldValue, pattern) {
+	var dateIndex =   pattern.indexOf("dd");
+	var dateValue = parseInt(dateFieldValue.substring(dateIndex,dateIndex + 2), 10);
+	var monthIndex =   pattern.indexOf("MM");
+	var monthValue = parseInt(dateFieldValue.substring(monthIndex,monthIndex + 2) - 1, 10);
+	var yearIndex =   pattern.indexOf("yyyy");
+	var yearValue = parseInt(dateFieldValue.substring(yearIndex,yearIndex + 4), 10);
+	var currentDate = new Date();
+	currentDate.setDate(dateValue);
+	currentDate.setMonth(monthValue);
+	currentDate.setYear(yearValue);
+	return currentDate;
+}
+
  _module.TimePicker = new TimePicker();
 return _module.TimePicker;
 
-})(base, gj);
+})(uiCalendar, base, gj);
