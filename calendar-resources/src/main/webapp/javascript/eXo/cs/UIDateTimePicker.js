40480c2486c6c85aa2a1227d48b0a54b80d2ea81
Tuan Pham <tuanp@exoplatform.com>
2013-03-08 18:52:09 +0700

    CAL-313 update css ui for minicalendar

--- calendar-resources/src/main/webapp/javascript/eXo/cs/UIDateTimePicker.js
+++ calendar-resources/src/main/webapp/javascript/eXo/cs/UIDateTimePicker.js
@@ -45,7 +45,7 @@ this.init = function(field, isDisplayTime) {
   // fix bug for IE 6
   var cld = document.getElementById(this.calendarId);
   if(base.Browser.isIE6())  {
-    var blockClnd = document.getElementById('BlockCaledar') ;
+    var blockClnd = document.getElementById('blockCaledar') ;
     var iframe = document.getElementById(this.calendarId + 'IFrame') ;
     iframe.style.height = blockClnd.offsetHeight + "px";
   }
@@ -179,62 +179,85 @@ this.renderCalendar = function() {
   var startDayOfWeek = this.getDayOfWeek(this.currentDate.getFullYear(), this.currentDate.getMonth() + 1, dayOfMonth) ;
   var daysInMonth = this.getDaysInMonth(this.currentDate.getFullYear(), this.currentDate.getMonth()) ;
   var clazz = null;
-  var table = '<div id="blockCaledar" class="BlockCalendar"><span></span></div>' ;
-  table +=    '<div class="UICalendar" onmousedown="event.cancelBubble = true">' ;
-  table +=    ' <table class="MonthYearBox">' ;
-  table +=    '   <tr>' ;
-  table +=    '     <td class="MonthButton"><a class="PreviousMonth" title="' + this.tooltip[1]+ '" href="javascript:eXo.cs.UIDateTimePicker.changeMonth(-1);"></a></td>' ;
-  table +=    '     <td class="YearButton"><a class="PreviousYear" title="' + this.tooltip[0]+ '" href="javascript:eXo.cs.UIDateTimePicker.changeYear(-1);"></a></td>' ;
-  table +=    '     <td><span style="color:#f89302;">' + this.months[this.currentDate.getMonth()] + '</span> - <span>' + this.currentDate.getFullYear() + '</span></td>' ;
-  table +=    '     <td class="YearButton"><a class="NextYear" title="' + this.tooltip[3]+ '"  href="javascript:eXo.cs.UIDateTimePicker.changeYear(1);"></a></td>' ;
-  table +=    '     <td class="MonthButton"><a class="NextMonth" title="' + this.tooltip[2]+ '" href="javascript:eXo.cs.UIDateTimePicker.changeMonth(1);"></a></td>' ;
-  table +=    '   </tr>' ;
-  table +=    ' </table>' ;
-  table +=    ' <div style="margin-top: 6px;padding: 0px 5px;">' ;
-  table +=    '   <table>' ;
-  table +=    '     <tr>' ;
-  table +=    '       <td><font color="red">' + this.weekdays[0] + '</font></td><td>' + this.weekdays[1] + '</td><td>' + this.weekdays[2] + '</td><td>' + this.weekdays[3] + '</td><td>' + this.weekdays[4] + '</td><td>' + this.weekdays[5] + '</td><td>' + this.weekdays[6] + '</td>' ;
-  table +=    '     </tr>' ;
-  table +=    '   </table>' ;
-  table +=    ' </div>' ;
-  table +=    ' <div class="CalendarGrid">' ;
-  table +=    ' <table>' ;  
+  var table = '<div id="blockCaledar"><span></span></div>' ;
+  table +=    '<div class="uiCalendarComponent uiBox" onmousedown="event.cancelBubble = true">';
+  table += '<h5 class="title clearfix">';
+  table += '<a data-placement="right" rel="tooltip" onclick="eXo.cs.UIDateTimePicker.changeMonth(-1);" class="actionIcon pull-left" data-original-title="'+ this.tooltip[1]+ '"><i class="uiIconMiniArrowLeft uiIconLightGray"></i></a>';
+  table += '<span>'+ this.months[this.currentDate.getMonth()] +', '+ this.currentDate.getFullYear() + '</span>';
+  table += '<a data-placement="right" rel="tooltip" onclick="eXo.cs.UIDateTimePicker.changeMonth(1);" class="actionIcon pull-right" data-original-title="'+ this.tooltip[2]+ '"><i class="uiIconMiniArrowRight uiIconLightGray"></i></a>';
+  table += '</h5>';
   
-  
-  for (var week=0; week < 6; week++) {
-    table += "<tr>";
-    for (var dayOfWeek=0; dayOfWeek < 7; dayOfWeek++) {
-      if (week == 0 && startDayOfWeek == dayOfWeek) {
-        validDay = 1;
-      } else if (validDay == 1 && dayOfMonth > daysInMonth) {
-        validDay = 0;
-      }
-      if (validDay) {
-        if (dayOfMonth == this.selectedDate.getDate() && this.currentDate.getFullYear() == this.selectedDate.getFullYear() && this.currentDate.getMonth() == this.selectedDate.getMonth()) {
-          clazz = 'Current';
-        } else if (dayOfWeek == 0 || dayOfWeek == 6) {
-          clazz = 'Weekend';
-        } else {
-          clazz = 'Weekday';
-        }
+  table += '<table class="weekList">';
+  table += '  <tr>';  
+  table +=    '       <td><font color="red">' + this.weekdays[0] + '</font></td><td>' + this.weekdays[1] + '</td><td>' + this.weekdays[2] + '</td><td>' + this.weekdays[3] + '</td><td>' + this.weekdays[4] + '</td><td>' + this.weekdays[5] + '</td><td>' + this.weekdays[6] + '</td>' ;  
+  table += '  </tr>';
+  table += '</table>';
+  table += '<hr>';
 
-        table = table + "<td><a class='"+clazz+"' href=\"javascript:eXo.cs.UIDateTimePicker.setDate("+this.currentDate.getFullYear()+","+(this.currentDate.getMonth() + 1)+","+dayOfMonth+")\">"+dayOfMonth+"</a></td>" ;
-        dayOfMonth++ ;
-      } else {
-        table = table + "<td class='empty'><div>&nbsp;</div></td>" ;
+var _pyear, _pmonth, _pday, _nyear, _nmonth, _nday, _weekend;
+    var _today = new Date();
+    var tableRow='';
+    if(startDayOfWeek==0) startDayOfWeek = 7;
+    _pyear = (this.currentDate.getMonth() == 0) ? this.currentDate.getFullYear() - 1 : this.currentDate.getFullYear();
+    _pmonth = (this.currentDate.getMonth() == 0) ? 11 : this.currentDate.getMonth() - 1;
+    _pday = this.getDaysInMonth(_pyear, _pmonth) - ((startDayOfWeek + ((8 - this.firstDayOfWeek) % 7)) % 7) + 1;
+    
+    _nmonth = (this.currentDate.getMonth() == 11) ? 0 : this.currentDate.getMonth() + 1;
+    _nyear = (this.currentDate.getMonth() == 11) ? this.currentDate.getFullYear() + 1 : this.currentDate.getFullYear();
+    _nday = 1;
+    
+    table += '<table cellspacing="0" cellpadding="0" id="" class="weekDays">';
+      for ( var week = 0; week < 6; week++) {
+      tableRow += '<tr {{week'+week+'}}>';
+        for ( var dayOfWeek = 0; dayOfWeek <= 6; dayOfWeek++) {
+          if (week == 0
+              && dayOfWeek == (startDayOfWeek + ((8 - this.firstDayOfWeek) % 7)) % 7) {
+            validDay = 1;
+          } else if (validDay == 1 && dayOfMonth > daysInMonth) {
+            validDay = 0;
+          }
+          if (validDay) {
+            if (dayOfMonth == this.selectedDate.getDate()
+                && this.currentDate.getFullYear() == this.selectedDate
+                    .getFullYear()
+                && this.currentDate.getMonth() == this.selectedDate.getMonth()) {
+              clazz = 'selected';
+            } else {
+              clazz = '';
+            }
+        if(_today.getDate() == dayOfMonth
+                && this.currentDate.getFullYear() == _today.getFullYear()
+                && this.currentDate.getMonth() == _today.getMonth()) {
+        clazz = 'highLight today';
+        tableRow = tableRow.replace('{{week'+week+'}}','class="currentWeek"');
+        }
+            tableRow = tableRow + '<td><a class="' + clazz
+                + '" href="#SelectDate" onclick="eXo.cs.UIDateTimePicker.setDate('
+                + this.currentDate.getFullYear() + ','
+                + (this.currentDate.getMonth() + 1) + ',' + dayOfMonth + ')">'
+                + dayOfMonth + '</a></td>';
+            dayOfMonth++;
+        _weekend = week;
+          } else if(validDay == 0 && week == 0) {
+            tableRow = tableRow + '<td><a href="#SelectDate" class="otherMonth" onclick="eXo.cs.UIDateTimePicker.setDate('
+                + _pyear + ','
+                + (_pmonth + 1) + ',' + _pday + ')">'
+                + _pday + '</a></td>';
+        _pday++;
+          } else if(validDay == 0 && week != 0 && _weekend==week){
+        tableRow = tableRow + '<td><a href="#SelectDate" class="otherMonth" onclick="eXo.cs.UIDateTimePicker.setDate('
+                + _nyear + ','
+                + (_nmonth + 1) + ',' + _nday + ')">'
+                + _nday + '</a></td>';
+        _nday++;
       }
-    }
-    table += "</tr>" ;
-  }   
-  table +=    '   </table>' ;
-  table +=    ' </div>' ;
-  if (this.isDisplayTime) {
-    table +=    ' <div class="CalendarTimeBox">' ;
-    table +=    '   <div class="CalendarTimeBoxR">' ;
-    table +=    '     <div class="CalendarTimeBoxM"><span><input class="InputTime" size="2" maxlength="2" value="' + this.currentDate.getHours() + '" onkeyup="eXo.cs.UIDateTimePicker.setHour(this)" >:<input size="2" class="InputTime" maxlength="2" value="' + this.currentDate.getMinutes() + '" onkeyup = "eXo.cs.UIDateTimePicker.setMinus(this)">:<input size="2" class="InputTime" maxlength="2" value="' + this.currentDate.getSeconds() + '" onkeyup = "eXo.cs.UIDateTimePicker.setSeconds(this)"></span></div>' ;
-    table +=    '   </div>' ;
-    table +=    ' </div>' ;
-  }
+        }
+        tableRow += "</tr>";
+      tableRow = tableRow.replace('{{week'+week+'}}','');
+      }
+      table += tableRow + '</table>';
+
+ 
   table +=    '</div>' ;
   //table +=    '</div>' ;
   return table ;
