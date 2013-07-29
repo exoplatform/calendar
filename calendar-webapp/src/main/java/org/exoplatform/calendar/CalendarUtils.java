a07880c7b0f817eb1cb99a2c1d7680eb1b404484
NgocTM <ngoctm@exoplatform.com>
2013-07-29 17:11:20 +0700

    CAL-560 HTTP status 404 when clicking on Link on invitation email of event on calendar of space
    Fix description: Only update url in case of intranet context in function getCalendarURL.

--- calendar-webapp/src/main/java/org/exoplatform/calendar/CalendarUtils.java
+++ calendar-webapp/src/main/java/org/exoplatform/calendar/CalendarUtils.java
@@ -555,9 +555,10 @@ public class CalendarUtils {
       if(url.indexOf(portalName + "/" + selectedNode) < 0){
         url = url.replaceFirst(portalName, portalName + "/" + selectedNode) ;
       } 
+      selectedNode = portalName + "/" + selectedNode;
+      url = url.substring(0, url.lastIndexOf(selectedNode) + selectedNode.length());
     } 
-    selectedNode = portalName + "/" + selectedNode;
-    url = url.substring(0, url.lastIndexOf(selectedNode) + selectedNode.length());
+    
     return url;
   }
 
