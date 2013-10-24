0b0f3d33bb62bc4698dcc3d03c8e5c64bbc18eb8
Tuan Pham <tuanp@exoplatform.com>
2013-10-24 16:32:27 +0700

    CAL-735 update icon after drop event

--- calendar-webapp/src/main/webapp/javascript/eXo/calendar/UIWeekView.js
+++ calendar-webapp/src/main/webapp/javascript/eXo/calendar/UIWeekView.js
@@ -397,7 +397,11 @@ UIWeekView.prototype.drop = function(evt) {
 	var _e = window.event || evt ;
 	var UIWeekView = _module.UIWeekView ;
 	var isEventbox = UIWeekView.dragElement;
-	if (!UIWeekView.isCol(_e) || !isEventbox) return ;
+    var repeatIcon = gj(isEventbox).find("i.uiIconCalRecurring") ;
+    if(repeatIcon.length != 0){
+        gj(repeatIcon).removeClass("uiIconCalRecurring").addClass("uiIconCalEditRecurring");
+    }
+    if (!UIWeekView.isCol(_e) || !isEventbox) return ;
 	var currentCol = UIWeekView.currentCol ;
 	var sourceCol = UIWeekView.dragElement.parentNode ;
 	var eventY = UIWeekView.eventY ;
