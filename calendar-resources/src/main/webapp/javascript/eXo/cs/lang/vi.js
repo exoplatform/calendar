e9f9879cb404391653388338a15c7a2e5e1ae6b4
trangvh <trangvh@exoplatform.com>
2013-11-22 19:59:36 +0700

    CAL-658 Add missing keys

--- calendar-resources/src/main/webapp/javascript/eXo/cs/lang/vi.js
+++ calendar-resources/src/main/webapp/javascript/eXo/cs/lang/vi.js
@@ -1,5 +1,5 @@
 [
 	['Tháng một','tháng hai','tháng ba','Tháng tư','tháng năm','tháng sáu','tháng bảy','tháng tám','tháng chín','tháng mười','tháng mười một','tháng mười hai'],
-	['C','H','B','B','N','S','B'],
+	['C','H','B','T','N','S','B'],
 	['Năm trước', 'Tháng trước', 'Tháng sau', 'Năm sau']
 ]
