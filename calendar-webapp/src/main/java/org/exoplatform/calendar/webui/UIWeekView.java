f2140fb8b73270917876a77f46ce114378cfeed6
nttuyen <tuyennt@exoplatform.com>
2014-07-24 16:57:03 +0700

    CAL-974 Many js error popup when drag/drop a recuring event in weekview

--- calendar-webapp/src/main/java/org/exoplatform/calendar/webui/UIWeekView.java
+++ calendar-webapp/src/main/java/org/exoplatform/calendar/webui/UIWeekView.java
@@ -700,12 +700,14 @@ public class UIWeekView extends UICalendarView {
             calendarview.refresh() ;
             UIMiniCalendar uiMiniCalendar = uiCalendarPortlet.findFirstComponentOfType(UIMiniCalendar.class) ;
             event.getRequestContext().addUIComponentToUpdateByAjax(uiMiniCalendar);
-            if(isOccur) event.getRequestContext().addUIComponentToUpdateByAjax(calendarview) ;
-            JavascriptManager jsManager = event.getRequestContext().getJavascriptManager();
-            RequireJS requireJS = jsManager.getRequireJS();
-            requireJS.require("PORTLET/calendar/CalendarPortlet","cal");
-            requireJS.addScripts("cal.UIWeekView.setSize();cal.UIWeekView.cleanUp();");
-
+            if(isOccur) {
+                event.getRequestContext().addUIComponentToUpdateByAjax(calendarview) ;
+            } else {
+                JavascriptManager jsManager = event.getRequestContext().getJavascriptManager();
+                RequireJS requireJS = jsManager.getRequireJS();
+                requireJS.require("PORTLET/calendar/CalendarPortlet","cal");
+                requireJS.addScripts("cal.UIWeekView.setSize();cal.UIWeekView.cleanUp();");
+            }
           }
         } catch (PathNotFoundException e) {
           if (log.isDebugEnabled()) {
