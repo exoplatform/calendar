455153a24689f65e6d97b1b541b3fee352e30d0c
Boubaker Khanfir <bkhanfir@exoplatform.com>
2018-02-09 19:26:08 +0100

    CAL-1311 Fix Allday Event display in Week view (#90)

--- calendar-webapp/src/main/webapp/javascript/eXo/calendar/EventObject.js
+++ calendar-webapp/src/main/webapp/javascript/eXo/calendar/EventObject.js
@@ -38,14 +38,20 @@ EventObject.prototype.init = function(rootNode) {
   this.eventIndex = this.rootNode.getAttribute('eventindex');
   this.calId = this.rootNode.getAttribute('calid');
   this.eventCat = this.rootNode.getAttribute('eventcat');
-  this.startTime = this.rootNode.getAttribute('starttime');
-  this.endTime = this.rootNode.getAttribute('endtime');
+  this.startTime = this.normalizeDate(this.rootNode.getAttribute('starttimefull'));
+  this.endTime = Date.parse(this.rootNode.getAttribute('endtimefull'));
 
   if (this.rootNode.innerText) {
     this.name = gj.trim(this.rootNode.innerText + '');
   } else {
     this.name = gj.trim(this.rootNode.textContent + '');
   }
+};
+
+EventObject.prototype.normalizeDate = function(dateStr) {
+  var d = new Date(dateStr);
+  if(document.getElementById("UIWeekView")) return Date.parse(dateStr);
+  return d.getTime();
 };
 
 EventObject.prototype.updateIndicator = function(nodeObj, hasBefore, hasAfter) {
