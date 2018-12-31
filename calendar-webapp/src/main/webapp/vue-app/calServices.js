4efa42f0e2ed1879e998ec38e345e8c6bbd9a687
Nguyen The Tuyen <tuyennt@exoplatform.com>
2018-12-31 09:19:36 +0700

    CAL-1358 add category and calendar select box

--- calendar-webapp/src/main/webapp/vue-app/calServices.js
+++ calendar-webapp/src/main/webapp/vue-app/calServices.js
@@ -1,4 +1,4 @@
-//import {calConstants} from './calConstants.js';
+import {calConstants} from './calConstants.js';
 
 export function findParticipants(filter, limit) {
 //  if(!limit) {
@@ -14,5 +14,50 @@ export function findParticipants(filter, limit) {
       {'id': 'john', 'name': 'john name', 'avatar': ''},
       {'id': 'mary', 'name': 'mary name', 'avatar': ''}
     ]);
+  });
+}
+
+export function getCategories() {
+  return fetch(`${calConstants.CAL_SERVER_API}categories`)
+    .then(resp =>  resp.json()).then(json => {
+      if (json && json.data) {
+        return json.data;
+      } else {
+        return [];
+      }
+    });
+}
+
+export function getCalendars() {
+  const groupMap = {
+    'PERSONAL': {'id': 0, 'name': 'privateCalendar'},
+    'SHARED': {'id': 1, 'name': 'sharedCalendar'},
+    'GROUP': {'id': 2, 'name': 'publicCalendar'}
+  }
+
+  return fetch(`${calConstants.CAL_SERVER_API}calendars`)
+    .then(resp =>  resp.json()).then(json => {
+      if (json && json.data) {
+        let calendarGroups = [];
+        //
+        json.data.forEach(calendar => {
+          let calGroup = calendarGroups.filter(group => group.type == calendar.type);
+          //
+          if (!calGroup.length) {
+            calGroup = [{
+              id: groupMap[calendar.type].id,
+              name: groupMap[calendar.type].name,
+              type: calendar.type,
+              calendars: []
+            }];
+            calendarGroups.push(calGroup[0]);
+          }
+          calGroup[0].calendars.push(calendar);
+        });
+
+        return calendarGroups;
+      } else {
+        return [];
+      }
   });
 }
