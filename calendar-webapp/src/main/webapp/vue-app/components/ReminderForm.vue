3f609321c9091fabd842a66db0b7afc6d6414b32
Nguyen The Tuyen <tuyennt@exoplatform.com>
2019-01-19 10:37:15 +0700

    CAL-1389 Clear button doesnt work in event popup

--- calendar-webapp/src/main/webapp/vue-app/components/ReminderForm.vue
+++ calendar-webapp/src/main/webapp/vue-app/components/ReminderForm.vue
@@ -8,7 +8,7 @@
       </div>
       <div v-show="mailReminder" class="selectboxSmall">
         <span class="uiSelectbox">
-          <select id="mailReminderTime" v-model="reminder.mailReminderTime" class="selectbox" name="mailReminderTime">
+          <select id="mailReminderTime" v-model="mailReminderTime" class="selectbox" name="mailReminderTime">
             <option v-for="i in [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60]" :key="i" :value="i">{{ i }} {{ $t('UIEventForm.label.minutes') }}</option>
           </select>
         </span>
