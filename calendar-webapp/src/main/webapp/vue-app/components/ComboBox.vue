eb53ca717756ff648805c6b6db85ced64714004d
Tran The Trong <trongtt@exoplatform.com>
2019-01-24 19:01:08 +0700

    CAL-1373 No list that contains time is displayed when click on From time and To time

--- calendar-webapp/src/main/webapp/vue-app/components/ComboBox.vue
+++ calendar-webapp/src/main/webapp/vue-app/components/ComboBox.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="cbb">
-    <input :disabled="disabled" :placeholder="placeholder" v-model="value" class="cbb_input" type="text" @click="showAutocompleteDropdown = true" @keyup.enter.prevent="select(selectedIndex)" @keydown.down.prevent="selectNext()" @keydown.up.prevent="selectPrev()" @keyup.8="handleBackspace()" />
+    <input :disabled="disabled" :placeholder="placeholder" v-model="value" class="cbb_input" type="text" @click="showOptions()" @keyup.enter.prevent="select(selectedIndex)" @keydown.down.prevent="selectNext()" @keydown.up.prevent="selectPrev()" @keyup.8="handleBackspace()" />
     <div class="cbb_list_wrapper">
       <ul v-if="showAutocompleteDropdown" class="cbb_list">
-        <li v-for="(value, index) in filteredOptions" :key="value" :class="{'selected': index == selectedIndex}" class="cbb_item" @click="select(index)">{{ value }}</li>
+        <li v-for="(value, index) in options" :key="value" :class="{'selected': isSelected(index)}" class="cbb_item" @click="select(index)">{{ value }}</li>
       </ul>
     </div>
   </div>
@@ -35,27 +35,24 @@ export default {
       selectedIndex: 0
     };
   },
-  computed: {
-    filteredOptions: function() {
-      const self = this;
-      return self.options.filter(function (opt) {
-        return opt.indexOf(self.value) !== -1;
-      });
-    }
-  },
   methods: {
+    isSelected: function(index) {
+      return this.value === this.options[index];
+    },
+    showOptions: function() {
+      this.showAutocompleteDropdown = true;
+    },
     handleBackspace: function () {
       this.showAutocompleteDropdown = true;
     },
     select: function(index) {
       this.showAutocompleteDropdown = false;
-      this.value = this.filteredOptions[index];
+      this.value = this.options[index];
       this.$emit('input', this.value);
     },
     selectNext: function() {
-
       if (this.showAutocompleteDropdown) {
-        if (this.selectedIndex < this.filteredOptions.length - 1) {
+        if (this.selectedIndex < this.options.length - 1) {
           this.selectedIndex++;
         } else {
           this.selectedIndex = 0;
@@ -69,7 +66,7 @@ export default {
       if (this.selectedIndex > 0) {
         this.selectedIndex--;
       } else {
-        this.selectedIndex = this.filteredOptions.length - 1;
+        this.selectedIndex = this.options.length - 1;
       }
     }
   }
