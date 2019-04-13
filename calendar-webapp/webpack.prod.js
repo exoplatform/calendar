5edd8f05562e411e9163aa08161683675e5fc56f
saraBoutej <sboutej@exoplatform.com>
2019-04-13 21:25:46 +0100

    Add webpack devtool option for developement and production mode

--- calendar-webapp/webpack.prod.js
+++ calendar-webapp/webpack.prod.js
@@ -5,7 +5,8 @@ const webpackCommonConfig = require('./webpack.common.js');
 let config = merge(webpackCommonConfig, {
   output: {
     path: path.resolve(__dirname, './target/calendar/')
-  }
+  },
+  devtool: 'cheap-module-eval-source-map'
 });
 
 module.exports = config;
